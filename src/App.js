5b21b56e966a98d661fa6915aeb9985937b3013d
AnandKrishnanKS <anandakrishnan1000@gmail.com>
2023-05-03 23:13:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './App.css';
-import { Container, Grid, TextField, ThemeProvider } from '@mui/material';
+import { Box, Button, Container, Divider, Grid, TextField, ThemeProvider } from '@mui/material';
 
 function App() {
   const form = useRef();
@@ -11,44 +11,59 @@ function App() {
 
     emailjs.sendForm('service_elw0d1v', 'template_hoftujj', form.current, 'tAz-lu9VPP0V4xVYx')
       .then((result) => {
-          if(result){
-            alert("mail sent success  ")
-            window.location.reload()
-          }else{
-            alert("something went wrong please try again.")
-            window.location.reload()
-          }
+        if (result) {
+          alert("mail sent success  ")
+          window.location.reload()
+        } else {
+          alert("something went wrong please try again.")
+          window.location.reload()
+        }
 
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-    }
+  }
 
-   
+
   return (
-   
-    <>
-        
-        <h1>hello trippy</h1>
 
-      <Container>
-      <form ref={form} onSubmit={sendEmail}>
-      <div style={{margin:"20px"}}>
-        <label>Name</label>
-        <TextField  type="text" name="user_name" />
-      </div>
-      <div style={{margin:"20px"}}>
-        <label>Email</label>
-        <TextField type="email" name="user_mail" />
-      </div>
-      <div style={{margin:"20px"}}>
-        <label>Message</label>
-        <textarea id="outlined-basic" name="message" />
-      </div>
-      <div><TextField type="submit" value="Send" /></div>
-    </form>
-      </Container>
-   
+    <>
+      
+      <Divider sx={{textAlign:"center",marginTop:"40px"}}>
+      <h1>hello trippy</h1>
+      </Divider>
+      
+      <Box sx={{
+        height:"60vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+
+
+        <form ref={form} onSubmit={sendEmail}>
+          <div >
+
+            <TextField sx={{width:"100%",marginBottom:"10px" }} required label="Name" type="text" name="user_name" />
+          </div>
+          <div >
+
+            <TextField sx={{width:"100%",marginBottom:"10px" }} required label="Email" type="email" name="user_mail" />
+          </div>
+          <div>
+
+            <TextField required
+             sx={{ width:"100%",marginBottom:"10px"}}
+              label="Enter text"
+              multiline
+              rows={5}
+              variant="outlined" name="message" />
+          </div>
+          <Divider sx={{alignItems:"center"}}><Button variant='contained' type="submit" value="Send" sx={{ text: "center" }}>Send</Button>
+          </Divider>
+        </form>
+      </Box>
+
 
     </>
   );
