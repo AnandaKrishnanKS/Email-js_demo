80be4822c3baf8790b6214b58efbf94b3fc2ecb1
AnandKrishnanKS <anandakrishnan1000@gmail.com>
2023-05-03 16:52:53 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './App.css';
+import { Container, Grid, TextField, ThemeProvider } from '@mui/material';
 
 function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_elw0d1v', 'template_hoftujj', form.current, 'tAz-lu9VPP0V4xVYx')
+      .then((result) => {
+          if(result){
+            alert("mail sent success  ")
+            window.location.reload()
+          }else{
+            alert("something went wrong please try again.")
+            window.location.reload()
+          }
+
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <>
+        
+        <h1>hello trippy</h1>
+
+      <Container>
+      <form ref={form} onSubmit={sendEmail}>
+      <div style={{margin:"20px"}}>
+        <label>Name</label>
+        <TextField  type="text" name="user_name" />
+      </div>
+      <div style={{margin:"20px"}}>
+        <label>Email</label>
+        <TextField type="email" name="user_mail" />
+      </div>
+      <div style={{margin:"20px"}}>
+        <label>Message</label>
+        <textarea id="outlined-basic" name="message" />
+      </div>
+      <div><TextField type="submit" value="Send" /></div>
+    </form>
+      </Container>
+   
+
+    </>
   );
 }
 
